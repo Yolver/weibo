fb9b5da02f7cc0100a7a72b950b35dfc4e688298
Yolver <735405853@qq.com>
2020-05-15 16:53:12 +0800

    发布微博

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        $user = Auth::user();
+        return view('static_pages/home',compact('user'));
     }
 
     public function help()
