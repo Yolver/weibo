7206ff1841f64b83785b78aa10b9e05f87951b14
Yolver <735405853@qq.com>
2020-05-15 17:10:40 +0800

    首页微博列表(更改)

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,18 +1,7 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-{{--  <div class="static-home">--}}
-{{--    <div>--}}
-{{--      @include('shared._status_form')--}}
-{{--    </div>--}}
-{{--    <div class="col-md-4">--}}
-{{--      <div class="offset-md-2 col-md-8">--}}
-{{--        <section class="user_info">--}}
-{{--          @include('shared._user_info',['user'=>$user])--}}
-{{--        </section>--}}
-{{--      </div>--}}
-{{--    </div>--}}
-{{--  </div>--}}
+
 @if(Auth::check())
   <div class="row">
     <div class="col-md-8">
