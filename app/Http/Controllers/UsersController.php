64493bf9d82b847fe3f219d61e489dc8b617de2f
Yolver <735405853@qq.com>
2020-05-15 15:27:15 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -107,13 +107,13 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'follw_heart@163.com';
-        $name = '随心音乐Music';
+//        $from = 'follw_heart@163.com';
+//        $name = '随心音乐Music';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱";
 
-        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 }
